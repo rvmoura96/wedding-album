49c5fc40a5f6f960d80bdc5e70e296c17f0ab7f5
Renan Moura <rvmoura.96@gmail.com>
2021-07-03 22:50:59 -0300

    - Adding submit photo feature; - E2E tests on submit a photo;

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
-from core.views import home, register
+from core.views import SubmitPhotoView, home, register
 from django.contrib.auth.views import LoginView
 from django.urls import path
 
 urlpatterns = [
-    path("register", register, name="register"),
-    path("login", LoginView.as_view(template_name="login.html"), name="login"),
     path("home", home, name="home"),
+    path("login", LoginView.as_view(template_name="login.html"), name="login"),
+    path("register", register, name="register"),
+    path("submit-photo", SubmitPhotoView.as_view(), name="submit-photo"),
 ]
